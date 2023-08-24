5f93a5740bcd21b5ec7b52c8c957aede458973c1
annamalai kathireshan <annamalaikathiresan@gmail.com>
2023-08-25 00:27:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import Validation from './utils/validation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  Sbbutton:any;
-  product: any;
-  title = 'POC_Project'; 
-  public productid!: number;
-  productname : string = " ";
+  form: FormGroup = new FormGroup({
+    productID: new FormControl(''),
+    productName: new FormControl(''),
+  });
+  submitted = false;
 
-  onSave($event: any){    
-    console.log("Save button is clicked!", $event);    
-  } 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        productID: ['', Validators.required],
+        productName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+      },
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    
+    if (this.form.invalid) {
+      return;
+    }
+    
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
